142cf5bb5c403af056957515948bfcbd1b436181
YanPoon <yan@hktaxiapp.com>
2024-11-22 18:54:06 +0800

    working update kotlin 1_3_40 & compileSDK 34

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/jeroentrappers/Developer/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/jeroentrappers/Developer/github/flutter_jailbreak_detection/example"
+export "FLUTTER_ROOT=/Users/yanpoon/fvm/versions/1.22.6"
+export "FLUTTER_APPLICATION_PATH=/Users/yanpoon/Documents/source_projects/GitHub/flutter_jailbreak_detection/example"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/home/jeroentrappers/Developer/flutter/bin/cache/artifacts/engine/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/yanpoon/fvm/versions/1.22.6/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
